645334f97ab5f96bb688987c4591e298effefde5
mi238959 <59659041+mi238959@users.noreply.github.com>
2020-03-24 20:36:55 -0400

    lab08

---
+++ lab8/pages/index.js
@@ -0,0 +1,15 @@
+import IdSearch from '../components/IdSearch'
+import NameSearch from '../components/NameSearch'
+
+const Search = () => {
+    return (
+      <div>
+        <IdSearch />
+        <NameSearch />
+        <div id="reportingArea"></div>
+      </div>
+    )
+  }
+
+
+export default Search;
